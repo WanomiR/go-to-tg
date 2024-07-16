490602eb436cdd75d8798c0d459b3f123d8672b1
Wanomir <ioan.romadin@gmail.com>
2024-07-16 13:30:44 +0300

    feat: inline keyboard

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -7,6 +7,19 @@ import (
 	"os"
 )
 
+var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonURL("1.com", "http://1.com"),
+		tgbotapi.NewInlineKeyboardButtonData("2", "2"),
+		tgbotapi.NewInlineKeyboardButtonData("3", "3"),
+	),
+	tgbotapi.NewInlineKeyboardRow(
+		tgbotapi.NewInlineKeyboardButtonData("4", "4"),
+		tgbotapi.NewInlineKeyboardButtonData("5", "5"),
+		tgbotapi.NewInlineKeyboardButtonData("6", "6"),
+	),
+)
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -20,39 +33,39 @@ func main() {
 
 	bot.Debug = true
 
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
 	updateConfig := tgbotapi.NewUpdate(0) // set initial offset
 	updateConfig.Timeout = 30             // wait up to 30 seconds on each request
 	updates := bot.GetUpdatesChan(updateConfig)
 
 	for update := range updates {
-		// Telegram can send many types of updates. For now, we only want to look at messages
-		if update.Message == nil {
-			continue
+		if update.Message != nil {
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+
+			switch update.Message.Text {
+			case "open":
+				msg.ReplyMarkup = numericKeyboard
+			}
+
+			if _, err = bot.Send(msg); err != nil {
+				log.Println(err)
+				continue
+			}
+		} else if update.CallbackQuery != nil {
+			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
+			if _, err := bot.Request(callback); err != nil {
+				log.Println(err)
+				continue
+			}
+
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
+			if _, err := bot.Send(msg); err != nil {
+				log.Println(err)
+				continue
+			}
 		}
 
-		// skip everything that is not a command
-		if !update.Message.IsCommand() {
-			continue
-		}
-
-		// now that we've gotten a message, construct a reply
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-
-		switch update.Message.Command() {
-		case "help":
-			msg.Text = "I understand /sayhi and /status"
-		case "sayhi":
-			msg.Text = "Hi :)"
-		case "status":
-			msg.Text = "I'm ok."
-		default:
-			msg.Text = "I don't know that command"
-		}
-
-		if _, err = bot.Send(msg); err != nil {
-			log.Println(err)
-			continue
-		}
 	}
 
 }
