e7147a12404af43838c51fd2844c5457d516af55
Wanomir <ioan.romadin@gmail.com>
2024-07-16 13:43:21 +0300

    feat: example code from official tutorial

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,71 +1,190 @@
 package main
 
 import (
-	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"context"
 	"github.com/joho/godotenv"
 	"log"
 	"os"
+	"strings"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-var numericKeyboard = tgbotapi.NewInlineKeyboardMarkup(
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonURL("1.com", "http://1.com"),
-		tgbotapi.NewInlineKeyboardButtonData("2", "2"),
-		tgbotapi.NewInlineKeyboardButtonData("3", "3"),
-	),
-	tgbotapi.NewInlineKeyboardRow(
-		tgbotapi.NewInlineKeyboardButtonData("4", "4"),
-		tgbotapi.NewInlineKeyboardButtonData("5", "5"),
-		tgbotapi.NewInlineKeyboardButtonData("6", "6"),
-	),
+var (
+	// Menu texts
+	firstMenu  = "<b>Menu 1</b>\n\nA beautiful menu with a shiny inline button."
+	secondMenu = "<b>Menu 2</b>\n\nA better menu with even more shiny inline buttons."
+
+	// Button texts
+	nextButton     = "Next"
+	backButton     = "Back"
+	tutorialButton = "Tutorial"
+
+	// Store bot screaming status
+	screaming = false
+	bot       *tgbotapi.BotAPI
+
+	// Keyboard layout for the first menu. One button, one row
+	firstMenuMarkup = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(nextButton, nextButton),
+		),
+	)
+
+	// Keyboard layout for the second menu. Two buttons, one per row
+	secondMenuMarkup = tgbotapi.NewInlineKeyboardMarkup(
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonData(backButton, backButton),
+		),
+		tgbotapi.NewInlineKeyboardRow(
+			tgbotapi.NewInlineKeyboardButtonURL(tutorialButton, "https://core.telegram.org/bots/api"),
+		),
+	)
 )
 
 func main() {
-	err := godotenv.Load()
+
+	err := godotenv.Load(".env")
 	if err != nil {
-		log.Fatal("error loading .env file")
+		log.Fatal("Error loading .env file", err)
+	}
+	bot, err = tgbotapi.NewBotAPI(os.Getenv("API_TOKEN"))
+	if err != nil {
+		// Abort if something is wrong
+		log.Fatal("Error creating bot", err)
 	}
 
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("API_TOKEN"))
-	if err != nil {
-		log.Fatal(err)
-	}
+	// Set this to true to log all interactions with telegram servers
+	bot.Debug = false
 
-	bot.Debug = true
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
 
-	log.Printf("Authorized on account %s", bot.Self.UserName)
+	// Create a new cancellable background context. Calling `cancel()` leads to the cancellation of the context
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
 
-	updateConfig := tgbotapi.NewUpdate(0) // set initial offset
-	updateConfig.Timeout = 30             // wait up to 30 seconds on each request
-	updates := bot.GetUpdatesChan(updateConfig)
+	// `updates` is a golang channel which receives telegram updates
+	updates := bot.GetUpdatesChan(u)
+	defer cancel()
 
-	for update := range updates {
-		if update.Message != nil {
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+	// Tell the user the bot is online
+	log.Println("Start listening for updates. Press enter to stop")
 
-			switch update.Message.Text {
-			case "open":
-				msg.ReplyMarkup = numericKeyboard
-			}
+	// Pass cancellable context to goroutine
+	receiveUpdates(ctx, updates)
 
-			if _, err = bot.Send(msg); err != nil {
-				log.Println(err)
-				continue
-			}
-		} else if update.CallbackQuery != nil {
-			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, update.CallbackQuery.Data)
-			if _, err := bot.Request(callback); err != nil {
-				log.Println(err)
-				continue
-			}
+	//// Wait for a newline symbol, then cancel handling updates
+	//bufio.NewReader(os.Stdin).ReadBytes('\n')
 
-			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Data)
-			if _, err := bot.Send(msg); err != nil {
-				log.Println(err)
-				continue
-			}
+}
+
+func receiveUpdates(ctx context.Context, updates tgbotapi.UpdatesChannel) {
+	// `for {` means the loop is infinite until we manually stop it
+	for {
+		select {
+		// stop looping if ctx is cancelled
+		case <-ctx.Done():
+			return
+		// receive update from channel and then handle it
+		case update := <-updates:
+			handleUpdate(update)
 		}
+	}
+}
 
+func handleUpdate(update tgbotapi.Update) {
+	switch {
+	// Handle messages
+	case update.Message != nil:
+		handleMessage(update.Message)
+		break
+
+	// Handle button clicks
+	case update.CallbackQuery != nil:
+		handleButton(update.CallbackQuery)
+		break
+	}
+}
+
+func handleMessage(message *tgbotapi.Message) {
+	user := message.From
+	text := message.Text
+
+	if user == nil {
+		return
 	}
 
+	// Print to console
+	log.Printf("%s wrote %s", user.FirstName, text)
+
+	var err error
+	if strings.HasPrefix(text, "/") {
+		err = handleCommand(message.Chat.ID, text)
+	} else if screaming && len(text) > 0 {
+		msg := tgbotapi.NewMessage(message.Chat.ID, strings.ToUpper(text))
+		// To preserve markdown, we attach entities (bold, italic..)
+		msg.Entities = message.Entities
+		_, err = bot.Send(msg)
+	} else {
+		// This is equivalent to forwarding, without the sender's name
+		copyMsg := tgbotapi.NewCopyMessage(message.Chat.ID, message.Chat.ID, message.MessageID)
+		_, err = bot.CopyMessage(copyMsg)
+	}
+
+	if err != nil {
+		log.Printf("An error occured: %s", err.Error())
+	}
+}
+
+// When we get a command, we react accordingly
+func handleCommand(chatId int64, command string) error {
+	var err error
+
+	switch command {
+	case "/scream":
+		screaming = true
+		break
+
+	case "/whisper":
+		screaming = false
+		break
+
+	case "/menu":
+		err = sendMenu(chatId)
+		break
+	}
+
+	return err
+}
+
+func handleButton(query *tgbotapi.CallbackQuery) {
+	var text string
+
+	markup := tgbotapi.NewInlineKeyboardMarkup()
+	message := query.Message
+
+	if query.Data == nextButton {
+		text = secondMenu
+		markup = secondMenuMarkup
+	} else if query.Data == backButton {
+		text = firstMenu
+		markup = firstMenuMarkup
+	}
+
+	callbackCfg := tgbotapi.NewCallback(query.ID, "")
+	bot.Send(callbackCfg)
+
+	// Replace menu text and keyboard
+	msg := tgbotapi.NewEditMessageTextAndMarkup(message.Chat.ID, message.MessageID, text, markup)
+	msg.ParseMode = tgbotapi.ModeHTML
+	bot.Send(msg)
+}
+
+func sendMenu(chatId int64) error {
+	msg := tgbotapi.NewMessage(chatId, firstMenu)
+	msg.ParseMode = tgbotapi.ModeHTML
+	msg.ReplyMarkup = firstMenuMarkup
+	_, err := bot.Send(msg)
+	return err
 }
